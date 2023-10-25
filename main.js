2e6409657892ccc72b6078322e2cc5787f8e58fa
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-10-25 14:34:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,25 @@ function setup()
 {
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function draw()
@@ -21,4 +40,10 @@ function draw()
     noFill();
     stroke("red");
     rect(30,60,450,350);
+
+    fill("red");
+    text("Cat",320,95);
+    noFill();
+    stroke("red");
+    rect(300,80,300,300);
 }
